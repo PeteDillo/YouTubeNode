fceffc6002ea67f3d9b42245856b94636603cc44
n-roberge <58243482+n-roberge@users.noreply.github.com>
2022-04-14 17:50:56 -0500

    all routes for comments are working

--- routes/comments.js
+++ routes/comments.js
@@ -20,9 +20,9 @@ router.get("/", async (req, res) => {
 //http://localhost:3007/api/comments/6254d8a0c8b02b63a73c49ad
 router.get("/:id", async (req, res) => {
     try {
-        let Comment = await Comment.findById(req.params.id);
+        let comment = await Comment.findById(req.params.id);
         if (!Comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
-        return res.send(Comment);
+        return res.send(comment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
@@ -31,18 +31,19 @@ router.get("/:id", async (req, res) => {
 //PUT update a Comment
 router.put("/:id", async (req, res) => {
     try {
-        let Comment = await Comment.findByIdAndUpdate(
+        let comment = await Comment.findByIdAndUpdate(
             req.params.id,
             {
                 name: req.body.name,
-                description: req.body.description,
-                category: req.body.category,
-                price: req.body.price,
+                body: req.body.body,
+                like: req.body.like,
+                dislike: req.body.dislike,
+                replies: req.body.replies
             },
             {new:true}
         );
-        if (!Comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
-        return res.send(Comment);
+        if (!comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        return res.send(comment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
@@ -52,10 +53,10 @@ router.put("/:id", async (req, res) => {
 //http://localhost:3007/api/comments/6254e000fbe3ae214584dba7
 router.delete("/:id", async (req, res) => {
     try {
-        let Comment = await Comment.findByIdAndDelete(req.params.id);
+        let comment = await Comment.findByIdAndDelete(req.params.id);
 
-        if (!Comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
-        return res.send(Comment);
+        if (!comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        return res.send(comment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
