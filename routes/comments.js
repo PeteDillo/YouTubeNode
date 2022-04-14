025458fa77d557575dba7205a0544e67fa277ecf
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-04-14 17:09:12 -0500

    updated comments.js to better fit the comment disc

--- routes/comments.js
+++ routes/comments.js
@@ -1,36 +1,36 @@
-const {Product, validateProduct} = require("../models/product");
+const {Comment, validateComment} = require("../models/Comment");
 const express = require("express");
 const {append} = require("express/lib/response")
 const router = express.Router();
 
-//GET all products
-//http://localhost:3007/api/products
+//GET all comments
+//http://localhost:3007/api/comments
 router.get("/", async (req, res) => {
     try {
-        let products = await Product.find();
-        if (!products) return res.status(400).send(`No products in this collection!`);
-        return res.send(products);
+        let comments = await Comment.find();
+        if (!comments) return res.status(400).send(`No comments in this collection!`);
+        return res.send(comments);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
 });
 
-//GET product by id
-//http://localhost:3007/api/products/6254d8a0c8b02b63a73c49ad
+//GET Comment by id
+//http://localhost:3007/api/comments/6254d8a0c8b02b63a73c49ad
 router.get("/:id", async (req, res) => {
     try {
-        let product = await Product.findById(req.params.id);
-        if (!product) return res.status(400).send(`Product with id "${req.params.id}" does not exist!`);
-        return res.send(product);
+        let Comment = await Comment.findById(req.params.id);
+        if (!Comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        return res.send(Comment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
 });
 
-//PUT update a product
+//PUT update a Comment
 router.put("/:id", async (req, res) => {
     try {
-        let product = await Product.findByIdAndUpdate(
+        let Comment = await Comment.findByIdAndUpdate(
             req.params.id,
             {
                 name: req.body.name,
@@ -40,36 +40,36 @@ router.put("/:id", async (req, res) => {
             },
             {new:true}
         );
-        if (!product) return res.status(400).send(`Product with id "${req.params.id}" does not exist!`);
-        return res.send(product);
+        if (!Comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        return res.send(Comment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
 });
 
-//DELETE a product
-//http://localhost:3007/api/products/6254e000fbe3ae214584dba7
+//DELETE a Comment
+//http://localhost:3007/api/comments/6254e000fbe3ae214584dba7
 router.delete("/:id", async (req, res) => {
     try {
-        let product = await Product.findByIdAndDelete(req.params.id);
+        let Comment = await Comment.findByIdAndDelete(req.params.id);
 
-        if (!product) return res.status(400).send(`Product with id "${req.params.id}" does not exist!`);
-        return res.send(product);
+        if (!Comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        return res.send(Comment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
 });
 
-//POST new product
+//POST new Comment
 router.post("/", async (req, res) => {
     try {
-        const {error} = validateProduct(req.body)
+        const {error} = validateComment(req.body)
         if (error) return res.status(400).send(error);
 
-        let newProduct = await new Product(req.body);
-        await newProduct.save();
+        let newComment = await new Comment(req.body);
+        await newComment.save();
 
-        return res.status(201).send(newProduct);
+        return res.status(201).send(newComment);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
