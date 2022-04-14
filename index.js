11b3862653c2e0b0d83487922eca851b70ca70b7
n-roberge <58243482+n-roberge@users.noreply.github.com>
2022-04-13 20:13:20 -0500

    added connection information

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const connectDb = require("./startup/db");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// const products = require("./routes/products");
+// const users = require("./routes/users.js");
+
+connectDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api/products', products)
+// app.use('/api/users',  users);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running. Listening on PORT: ${PORT}`);
+});
